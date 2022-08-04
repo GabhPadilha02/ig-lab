c474108bfb4d75bdfd06d20d8be6675e67af30bf
gabhpadilha02 <gabh.padilha2@gmail.com>
2022-08-04 16:47:37 -0300

    adicionando conteudo a sessão vazia

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,5 +1,7 @@
+import { RocketLaunch } from "phosphor-react";
 import { useParams } from "react-router-dom";
 import { Header } from "../components/Header";
+import { ReactLogo } from "../components/ReactLogo";
 import { Sidebar } from "../components/Sidebar";
 import { Video } from "../components/Video";
 
@@ -12,7 +14,16 @@ export function Event() {
         <main className="flex flex-1">
           {slug 
           ? <Video lessonSlug={slug}/> 
-          : <div className="flex-1"/>}
+          : <div className="flex-1 items-center justify-center flex flex-col">
+              <div className="text-center flex m-10 w-3/4 justify-center text-gray-200 ">
+                <span className="text-4xl">Acesse o <strong className="text-blue-500">menu</strong>, escolha sua aula, e embarque nessa nova jornada!</span>
+                <RocketLaunch size={100} className="text-blue-500 md:w-2/3 md:relative md:bottom-5 md:left-5" />
+              
+              </div>
+               <ReactLogo/>
+            </div> 
+            }
+            
           <Sidebar/>
         </main>
       </div>
